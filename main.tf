e8d03cdf0989feb15fccadba44bf4212db31b984
deepf4dev <deepf4dev@gmail.com>
2023-12-17 00:54:53 +0100

    feat: added s3 bucket

--- main.tf
+++ main.tf
@@ -3,32 +3,4 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-resource "aws_s3_bucket" "hosting_bucket" {
-  bucket = var.bucket_name
-}
-
-resource "aws_s3_bucket_policy" "hosting_bucket_policy" {
-  bucket = aws_s3_bucket.hosting_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = "s3:GetObject",
-        Resource  = "arn:aws:s3:::${aws_s3_bucket.hosting_bucket.id}/*"
-      }
-    ]
-  })
-}
-
-
-resource "aws_s3_bucket_website_configuration" "hosting_bucket_website_configuration" {
-  bucket = aws_s3_bucket.hosting_bucket.id
-
-  index_document {
-    suffix = "index.html"
-  }
-}
 

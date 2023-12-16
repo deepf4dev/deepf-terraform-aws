d05d8f9396c3d3c906d6360d974223ed95a91d02
deepf4dev <142343821+deepf4dev@users.noreply.github.com>
2023-12-16 23:10:58 +0000

    Feature/add resources (#2)
    * add first awd resouce
    
    * add first aws resource
    
    * feat: added s3 bucket for the react app

--- main.tf
+++ main.tf
@@ -3,6 +3,36 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "mon-nom-de-bucket-unique"
+resource "aws_s3_bucket" "hosting_bucket" {
+  bucket = var.bucket_name
 }
+
+resource "aws_s3_bucket_acl" "hosting_bucket_acl" {
+  bucket = aws_s3_bucket.hosting_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "hosting_bucket_policy" {
+  bucket = aws_s3_bucket.hosting_bucket.id
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : "*",
+        "Action" : "s3:GetObject",
+        "Resource" : "arn:aws:s3:::${var.bucket_name}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_website_configuration" "hosting_bucket_website_configuration" {
+  bucket = aws_s3_bucket.hosting_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
